887acc78a48dfdaa394c4d87cbf9ad8c3e9d39a3
Lazar Stanojević <lazar.stanojevic@pmf.edu.rs>
2016-09-20 03:39:39 +0200

    Dodat meni i toolbar ikona

--- app/src/main/java/rs/laxsrbija/piro/MainActivity.java
+++ app/src/main/java/rs/laxsrbija/piro/MainActivity.java
@@ -3,6 +3,8 @@ package rs.laxsrbija.piro;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
@@ -12,12 +14,22 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.mipmap.ic_icon);
+
         DataLoadTask.displayStoredData(this);
         new DataLoadTask(MainActivity.this, DataLoadTask.MODE_REGULAR).execute();
 
         ((SwipeRefreshLayout) findViewById(R.id.swipeContainer)).setOnRefreshListener(this);
         PiroOnClickListener.setOnClickListener(MainActivity.this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     @Override
